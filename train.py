60973a1b07691fccd210e5381334e6d54c810265
turcuciprian <turcuciprian1@gmail.com>
2024-01-13 13:58:43 +0200

    added numpy added load_group function

--- train.py
+++ train.py
@@ -1,6 +1,19 @@
 from pandas import read_csv
+from numpy import dstack
+
 
 # load a single file as a numpy array
 def load_file(filepath):
-    dataframe = read_csv(filepath, header=None, delim_whitespace==True)
+    dataframe = read_csv(filepath, header=None, delim_whitespace=True)
     return dataframe.values
+
+
+# load list of files into a 3D array of [samples, timesteps, features]
+def load_group(filenames, prefix=""):
+    loaded = list()
+    for name in filenames:
+        data = load_file(prefix + name)
+        loaded.append(data)
+    # stack group so that features are the 3rd dimension
+    loaded = dstack(loaded)
+    return loaded
